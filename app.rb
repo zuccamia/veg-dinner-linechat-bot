4939fcb6c532e3e3cdeabdea239f233c88ac6b7b
Ngoc Hoang <zuccamia@gmail.com>
2020-11-02 00:17:11 +0900

    fix open url

--- app.rb
+++ app.rb
@@ -45,7 +45,7 @@ def suggest_meal_idea
 end
 
 def search_that(message)
-  ecosia_first_page = Nokogiri::HTML(open("https://www.ecosia.org/search?q=#{message.gsub(/'/, '').gsub(/\s/, '+')}").read)
+  ecosia_first_page = Nokogiri::HTML(open(CGI.escape("https://www.ecosia.org/search?q=#{message}").read))
   ecosia_first_page.search('.result-url[href]')[0].attribute('href').value
 end
 
