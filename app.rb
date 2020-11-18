40a622a4b0892d79ae047ad5f981a1d1eb10d9ba
Ngoc Hoang <zuccamia@gmail.com>
2020-11-18 17:46:48 +0900

    debug json

--- app.rb
+++ app.rb
@@ -49,7 +49,7 @@ end
 def search_that(message)
   json = RestClient.get(URI.escape("https://api.duckduckgo.com/?q=#{message}&format=json&pretty=1"))
   ""
-  p json
+  p JSON.parse(json)
   ""
   text = JSON.parse(json)['RelatedTopics'].first['Text']
   url = JSON.parse(json)['RelatedTopics'].first['FirstURL']
